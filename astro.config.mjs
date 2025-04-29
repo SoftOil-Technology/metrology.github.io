10d80f815b62087f116b6533479214cea80438a9
adm-softoil <138618032+adm-softoil@users.noreply.github.com>
2025-04-29 16:25:45 -0600

    Configure Astro for GH Pages deployment and add Actions workflow

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,18 @@
+// astro.config.mjs
+
 // @ts-check
 import { defineConfig } from 'astro/config';
-import tailwindcss from '@tailwindcss/vite';
+import tailwindcss from '@tailwindcss/vite'; // Tu plugin existente
 
 // https://astro.build/config
 export default defineConfig({
+  
+  site: 'https://www.kepler-metrology.com.mx',
+  base: '/',
+  output: 'static',
   vite: {
     plugins: [tailwindcss()]
   },
-
   i18n: {
     defaultLocale: 'es',
     locales: ['es', 'en'],
